b3c7c97e8bc96c1c8199d348876482bb5a6ff71f
crazy2010king <crazy2010king@a153f2b5-eae4-7df0-78f6-d2343ce1c61a>
2011-12-14 12:27:48 +0000

    第一部分开始：公共头文件， 1.提交修改

--- ffmpeg0.7-CKW/ffmpeg-git/libavutil/attributes.h
+++ ffmpeg0.7-CKW/ffmpeg-git/libavutil/attributes.h
@@ -23,6 +23,14 @@
  * Macro definitions for various function/variable attributes
  */
 
+//****************************************************************************//
+//attributes.h：屏蔽不同编译器的区别（主要是GCC不同版本的区别）
+//要学习的地方：
+//1.GCC的版本表示手法：__GNUC__表示主版本号， __GNUC_MINOR__表示分支版本号
+//参考资料：
+//附录
+//1.扩展知识----GNU C 扩展之__attribute__ 机制简介.txt
+//****************************************************************************//
 #ifndef AVUTIL_ATTRIBUTES_H
 #define AVUTIL_ATTRIBUTES_H
 
@@ -32,40 +40,39 @@
 #define FFMPEGLIB_API_IN   __declspec(dllimport)
 #endif
 
-
 #ifdef __GNUC__
-#    define AV_GCC_VERSION_AT_LEAST(x,y) (__GNUC__ > x || __GNUC__ == x && __GNUC_MINOR__ >= y)
+#	define AV_GCC_VERSION_AT_LEAST(x, y)	(__GNUC__ > x || __GNUC__ == x && __GNUC_MINOR__ >= y) 
 #else
-#    define AV_GCC_VERSION_AT_LEAST(x,y) 0
+#	define AV_GCC_VERSION_AT_LEAST(x, y)	0
 #endif
 
 #ifndef av_always_inline
 #if AV_GCC_VERSION_AT_LEAST(3,1)
-#    define av_always_inline __attribute__((always_inline)) inline
+#	define av_always_inline	__attribute__((always_inline)) inline
 #else
-#    define av_always_inline inline
+#	define av_always_inline inline
 #endif
 #endif
 
 #ifndef av_noinline
 #if AV_GCC_VERSION_AT_LEAST(3,1)
-#    define av_noinline __attribute__((noinline))
+#	define av_noinline __attribute__((noinline))
 #else
-#    define av_noinline
+#	define av_noinline
 #endif
 #endif
 
 #ifndef av_pure
 #if AV_GCC_VERSION_AT_LEAST(3,1)
-#    define av_pure __attribute__((pure))
-#else
-#    define av_pure
+#	define av_pure __attribute__((pure))
+#else 
+#	define av_pure
 #endif
 #endif
 
 #ifndef av_const
 #if AV_GCC_VERSION_AT_LEAST(2,6)
-#    define av_const __attribute__((const))
+#	define av_const __attribute__((const))
 #else
 #    define av_const
 #endif
@@ -73,33 +80,33 @@
 
 #ifndef av_cold
 #if AV_GCC_VERSION_AT_LEAST(4,3)
-#    define av_cold __attribute__((cold))
+#	define av_cold __attribute__((cold))
 #else
-#    define av_cold
+#	define av_cold
 #endif
 #endif
 
 #ifndef av_flatten
 #if AV_GCC_VERSION_AT_LEAST(4,1)
-#    define av_flatten __attribute__((flatten))
+#	define av_flatten __attribute__((flatten))
 #else
-#    define av_flatten
+#	define av_flatten
 #endif
 #endif
 
 #ifndef attribute_deprecated
 #if AV_GCC_VERSION_AT_LEAST(3,1)
-#    define attribute_deprecated __attribute__((deprecated))
-#else
-#    define attribute_deprecated
+#	define attribute_deprecated __attribute__((deprecated))
+#else 
+#	define attribute_deprecated
 #endif
 #endif
 
 #ifndef av_unused
 #if defined(__GNUC__)
-#    define av_unused __attribute__((unused))
+#	define av_unused __attribute__((unused))
 #else
-#    define av_unused
+#	define av_unused
 #endif
 #endif
 
@@ -110,32 +117,32 @@
  */
 #ifndef av_used
 #if AV_GCC_VERSION_AT_LEAST(3,1)
-#    define av_used __attribute__((used))
-#else
-#    define av_used
+#	define av_used __attribute__((used))
+#else 
+#	define av_used
 #endif
 #endif
 
 #ifndef av_alias
 #if AV_GCC_VERSION_AT_LEAST(3,3)
-#   define av_alias __attribute__((may_alias))
+#	define av_alias __attribute__((may_alias))
 #else
-#   define av_alias
+#	define av_alias
 #endif
 #endif
 
 #ifndef av_uninit
 #if defined(__GNUC__) && !defined(__INTEL_COMPILER)
-#    define av_uninit(x) x=x
+#	define av_uninit(x)	x=x
 #else
-#    define av_uninit(x) x
+#	define av_uninit(x)	x
 #endif
 #endif
 
 #ifdef __GNUC__
-#    define av_builtin_constant_p __builtin_constant_p
+#	define av_builtin_constant_p __builtin_constant_p
 #else
-#    define av_builtin_constant_p(x) 0
+#	define av_builtin_constant_p(x)	0
 #endif
 
 #endif /* AVUTIL_ATTRIBUTES_H */
